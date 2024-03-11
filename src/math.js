776e8945c767dec1cc394e72bb05cd1c83a54768
thmsousa <129226086+thmsousa@users.noreply.github.com>
2024-03-10 22:07:15 -0300

    Add files via upload

--- src/math.js
+++ src/math.js
@@ -8,7 +8,10 @@
  * @param {Array} numeros vetor com os números desejados
  */
 function somaDobroNumerosParesFuncional(numeros){
-    // TODO: Insira seu código funcional aqui
+    return numeros
+        .filter(num => num % 2 === 0)
+        .map(num => num * 2)
+        .reduce((acc, curr) => acc + curr, 0);
 }
 
 /**
@@ -17,7 +20,13 @@ function somaDobroNumerosParesFuncional(numeros){
  * @param {Array} numeros vetor com os números desejados
  */
 function somaDobroNumerosParesProcedural(numeros){
-    // TODO: Insira seu código estruturado aqui   
+    let soma = 0;
+    for (let i = 0; i < numeros.length; i++) {
+        if (numeros[i] % 2 === 0) {
+            soma += numeros[i] * 2;
+        }
+    }
+    return soma;
 }
 
 export { somaDobroNumerosParesFuncional, somaDobroNumerosParesProcedural };
